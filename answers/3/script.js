4986f8f031c9797d51bdadf7b5e7442ae850b4ac
RoiMekaiton <roimekaiton@gmail.com>
2025-03-05 00:58:01 +0200

    ex3
    Signed-off-by: RoiMekaiton <roimekaiton@gmail.com>

---
+++ answers/3/script.js
@@ -0,0 +1,11 @@
+// ex1 ->
+const base = prompt("Enter a base:")
+const height = prompt("Enter a height:")
+alert(`The area of the triangle is ${base * height / 2}`)
+
+// ex14->
+const now = new Date()
+
+console.log(`${now.getFullYear()}-${now.getMonth()}-${now.getDay()} ${now.getHours()}:${now.getMinutes()}`)
+console.log(`${now.getDay()}-${now.getMonth()}-${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}`)
+console.log(`${now.getDay()}/${now.getMonth()}/${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}`)
